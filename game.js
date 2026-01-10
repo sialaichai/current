5e1f8233e6cee09bf2a868cf25ffe76ee3dbce9a
sialaichai <sialaichai@gmail.com>
2026-01-10 22:25:00 +0800

    Update game.js

--- game.js
+++ game.js
@@ -741,7 +741,10 @@ checkDoorCollision() {
       if (!this.state.questionAnswered && !this.state.currentQuestion) {
         this.loadQuestion();
       }
-    } else if (!door.locked && this.isCollidingRectRect(...)) {
+        else if (!door.locked && this.isCollidingRectRect(
+      this.player.x, this.player.y, this.player.width, this.player.height,
+      door.x, door.y, door.width, door.height
+    )) {
       this.completeLevel();
     }
   });
